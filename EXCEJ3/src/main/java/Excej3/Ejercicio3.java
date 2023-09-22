0ec5f4c06d4cb4b208aab625a0ce9bafa34b3369
Evocuer <ing.cuervodiaz@gmail.com>
2023-09-21 19:07:01 -0500

    Ejercicio 4 adicionado

--- EXCEJ3/src/main/java/Excej3/Ejercicio3.java
+++ EXCEJ3/src/main/java/Excej3/Ejercicio3.java
@@ -12,19 +12,26 @@ public class Ejercicio3 {
 
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
         DivisionNumero dn = new DivisionNumero();
+        int num1 = 0;
+        int num2 = 0;
+
         System.out.println("Ingrese dos numeros a operarse continuacion");
         System.out.println("Primer Numero");
-        String num1 = leer.next();
+        String n1 = leer.next();
 
         System.out.println("Segundo Numero");
-        String num2 = leer.next();
+        String n2 = leer.next();
+        try {
+            num1 = parseInt(n1);
+            num2 = parseInt(n2);
+        }  catch (NumberFormatException e) {
+            System.out.println("Error: ingreso invalido");
+        }
 
         try{
-            System.out.println("La Division de: " + num1 + " entre " + num2 + " = " +dn.DivisionNumero(parseInt(num1), parseInt(num2)));
+            System.out.println("La Division de: " + num1 + " entre " + num2 + " = " +dn.DivisionNumero(num1, num2));
         } catch (ArithmeticException e) {
-
             System.out.println("Error: division por cero");
-
         }
 
     }
